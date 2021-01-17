b6bf463abf1d79f65eaa7262db88a4cc9aa78391
Shopeyin Wale <shopeyinwale@gmail.com>
2021-01-17 23:55:05 +0100

    initial commit :rocket:

---
+++ index.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const axios = require("axios");
+
+const app = express();
+
+const getBase = async (req, res) => {
+  try {
+    const base = req.query.base;
+    const cur = req.query.currency;
+    let data;
+    if (base && cur) {
+      data = await axios.get(
+        `https://api.exchangeratesapi.io/latest?base=${base}&symbols=${cur.toUpperCase()}`
+      );
+      const result = data.data;
+      res.json(result);
+    } else if (base) {
+      data = await axios.get(
+        `https://api.exchangeratesapi.io/latest?base=${base}`
+      );
+      const result = data.data;
+      res.json(result);
+    }
+    data = await axios.get(`https://api.exchangeratesapi.io/latest`);
+    const result = data.data;
+    res.json(result);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+app.get("/", (req, res) => {
+  res.send("Welcome to Enye-Tech Backend Challenge");
+});
+
+app.get("/api/rates", getBase);
+
+app.all("*", (req, res, next) => {
+  res.send(`Can't find ${req.originalUrl} on this server!`);
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`App running on port ${port}...`);
+});
